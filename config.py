0eeeec902b34342a6b0eaf135a91297139b8a31a
the-noob-coder69 <shantnuprofid@gmail.com>
2021-03-04 18:35:12 +0000

    added main function and a config file
    Signed-off-by: the-noob-coder69 <shantnuprofid@gmail.com>

---
+++ config.py
@@ -0,0 +1,2 @@
+#path of your skinpack folder
+path='/storage/emulated/0/games/com.mojang/skin_packs/Shantnu\'s skin pack/'
--- skinCreator.py
+++ skinCreator.py
@@ -1,10 +1,8 @@
 import json,os
 
-path='/storage/emulated/0/games/com.mojang/skin_packs/Shantnu\'s skin pack/'
-f=open(path+"skins.json",'r')
 
 def load_skinjson(file):
-    full_data=json.load(f)
+    full_data=json.load(file)
     skins=full_data['skins']
 
     skin_names=[]
@@ -12,3 +10,8 @@ def load_skinjson(file):
         skin_names.append(i['localization_name'])
 
     print('full_data',full_data,'skins',skins,'skin_names',skin_names,sep='\n\n\n\n\n\n')
+
+def main():
+    from config import path
+    f=open(path+"skins.json",'r')
+    load_skinjson(f)
