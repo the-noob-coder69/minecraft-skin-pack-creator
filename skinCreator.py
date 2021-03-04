2b92baa84dbd9ff030a6ed01e33b91246b533c6c
the-noob-coder69 <shantnuprofid@gmail.com>
2021-03-04 18:25:22 +0000

    organized a little bit
    Signed-off-by: the-noob-coder69 <shantnuprofid@gmail.com>

--- skinCreator.py
+++ skinCreator.py
@@ -1,14 +1,14 @@
-
 import json,os
 
 path='/storage/emulated/0/games/com.mojang/skin_packs/Shantnu\'s skin pack/'
 f=open(path+"skins.json",'r')
 
-full_data=json.load(f)
-skins=full_data['skins']
+def load_skinjson(file):
+    full_data=json.load(f)
+    skins=full_data['skins']
 
-skin_names=[]
+    skin_names=[]
+    for i in skins:
+        skin_names.append(i['localization_name'])
 
-for i in skins:
-    skin_names.append(i['localization_name'])
-
+    print('full_data',full_data,'skins',skins,'skin_names',skin_names,sep='\n\n\n\n\n\n')
