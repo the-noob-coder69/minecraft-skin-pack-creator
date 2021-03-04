5674f32bf7566c1ddd2c96c22af3aea6fc3fd9e1
the-noob-coder69 <shantnuprofid@gmail.com>
2021-03-04 21:19:28 +0000

    added some functions for creating json file. Not implemented yet
    Signed-off-by: the-noob-coder69 <shantnuprofid@gmail.com>

--- skinCreator.py
+++ skinCreator.py
@@ -1,7 +1,8 @@
 import json,os
-
+import re
 
 def load_skinjson(file):
+    print('loading file')
     full_data=json.load(file)
     skins=full_data['skins']
 
@@ -11,7 +12,38 @@ def load_skinjson(file):
 
     print('full_data',full_data,'skins',skins,'skin_names',skin_names,sep='\n\n\n\n\n\n')
 
+def skins(path) -> list:
+    lst=[]
+    print(os.listdir(path))
+    for i in os.listdir(path):
+        if i[-3:]=='png':
+            for j in ["geometry.humanoid.custom", "geometry.humanoid.customSlim"]:
+                if j[-4:]=='Slim': lst.append(skin(i[:-4]+' slim',j,i,'free'))
+                else: lst.append(skin(i[:-4],j,i,'free'))
+    return lst
+
+def skin(name: str,geo: str,texture: str,type: str) -> dict:
+    return {"localization_name":name,"geometry":geo,"texture":texture,"type":type}
+
+def create_skinjsonfile(path: str,sname: str,lname: str) -> dict:
+    """json file format :-
+    {
+        "skins":[
+            {
+                "localization_name":...,
+                "geometry":...,
+                "texture":...,
+                "type":...
+            }
+        ],
+        "serialize_name":...,
+        "localization_name":...
+    }
+    """
+    return {"skins":skins(path),"serialize_name":sname,"localization_name":lname}
+
 def main():
     from config import path
     f=open(path+"skins.json",'r')
     load_skinjson(f)
+main()
